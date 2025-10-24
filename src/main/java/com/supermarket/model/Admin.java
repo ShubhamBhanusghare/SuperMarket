86fd8b745aaea8cd1fb320d3659e436a971035b5
Shubham <bshubham.ac@gmail.com>
2025-10-24 11:24:28 +0530

    Story3: Repository Functions updated

--- src/main/java/com/supermarket/model/Admin.java
+++ src/main/java/com/supermarket/model/Admin.java
@@ -2,7 +2,8 @@ package com.supermarket.model;
 
 public class Admin extends User{
 
-    public Admin(String userId, String userName, String password,Roles roles) {
-        super(userId, userName, password, roles);
+    public Admin(String userId, String userName, String password) {
+        super(userId, userName, password, Roles.ADMIN);
+
     }
 }
